5da3a18164ddebea33bd116380fe5deddb736e03
Minerva <superetro85@gmail.com>
2024-09-13 10:51:03 -0500

    Add: Local Storage parse to JSON

--- src/js/localstorage.js
+++ src/js/localstorage.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-    readAll()
+    // readAll()
 }
 
 // Make data
@@ -7,7 +7,24 @@ let data = [
     {id: 0, name: 'Minerva', last_name: 'Alves', gender: 'M', birthday: '20-06-2005', email: 'superetro85@gmail.com'}
 ]
 
-// Verificar se tem presença no LocalStorage, se nao gouver, será criado
+// Converter o array de objeto
+const jsonData = JSON.stringify(data)
+
+// Passar o dado para o Local Storage, mesmo se tiver vazio
 if (!localStorage.getItem('people')){
-    localStorage.setItem('people', JSON.stringify(data));
+    localStorage.setItem('people', jsonData);
 }
+
+// Passar o data para JSON
+const storedData = localStorage.getItem('people')
+const obj_data = JSON.parse(storedData);
+
+
+
+// Para ter acesso aos atributos, é necessário Iterar... Pegar um de cada
+// obj_data.forEach(item => {
+//     console.log(`ID: ${item.id}, name: ${item.name}, last_name: ${item.last_name}`)
+// })
+
+
+console.log(obj_data.name)
